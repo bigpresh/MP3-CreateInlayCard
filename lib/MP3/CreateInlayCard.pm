afa9c7f747a25d480408bbc90d93c2f47b3ad6ca
David Precious <davidp@preshweb.co.uk>
2009-02-11 10:09:57 +0000

    Use Music::Tag instead of MP3::Tag.  Last version generated lots of failures since it declared dependence on MP3::Tag, but not MP3::Info, which MP3::Tag requires to do anything useful, and dies if it's not present.
    Using Music::Tag means I can release a future version which supports various
    file types (at which point the module name will be a misnomer, arse).
    
    Also, generate META.yml automatically, rather than manually (which sucks).
    
    
    git-svn-id: file:///shared/svn/scripts/projects/perl/modules/MP3-CreateInlayCard@530 c1e6fe2f-0919-0410-96f0-e3221d39872c

--- lib/MP3/CreateInlayCard.pm
+++ lib/MP3/CreateInlayCard.pm
@@ -1,13 +1,13 @@
 package MP3::CreateInlayCard;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 # $Id$
 
 use strict;
 use warnings;
 use File::Find::Rule;
-use MP3::Tag;
+use Music::Tag;
 use Cwd;
 use HTML::Template;
 
@@ -107,25 +107,23 @@ sub create_inlay {
     for my $file (@files) {
 
         my $mp3;
-        eval { $mp3 = MP3::Tag->new($file); };
+        my $tags = Music::Tag->new($file);
+        $tags->get_tag or warn "Error reading tags from $file" and next;
 
-        if (!$mp3) { warn "Error reading $file\n"; next; }
-
-
-        my $length = sprintf('%02d',$mp3->total_secs / 60) . ':'
-            . sprintf('%02d',$mp3->total_secs % 60);
+        my $length = sprintf('%02d',$tags->secs / 60) . ':'
+            . sprintf('%02d',$tags->secs % 60);
 
         push @tracks, {
             track => $track,
             title => $params->{prettify} ?
-                _prettify($mp3->title()) : $mp3->title(),
+                _prettify($tags->title()) : $tags->title(),
             artist => $params->{pretify} ?
-                _prettify($mp3->artist()) : $mp3->artist(),
+                _prettify($tags->artist()) : $tags->artist(),
             length => $length,
         };
 
-        $artists{ $mp3->artist() }++;
-        $albums{  $mp3->album()  }++;
+        $artists{ $tags->artist() }++;
+        $albums{  $tags->album()  }++;
         $track++;
     }
 
